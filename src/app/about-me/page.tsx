77955fba7143590a4ef469bf09746690df796801
Khanh Viet <vietkhanh1310@gmail.com>
2023-02-22 10:23:15 +0700

    update

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 const AboutMe = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const khanh: string = "ddas";
+  return <h1 className="text-3xl font-bold underline">Hello world! {khanh}</h1>;
 };
 
 export default AboutMe;
