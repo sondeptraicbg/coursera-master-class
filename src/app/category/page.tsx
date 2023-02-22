ab7eee42558f2f5417a86c0cdcd7a7d88fab85cd
hiepnh <hiepnh@rikkeisoft.com>
2023-02-22 16:28:09 +0700

    Add home page

---
+++ src/app/category/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+const Category = () => {
+    return (
+        <>
+          <div className="columns-2">
+            <div className="w-full aspect-video">1</div>
+            <div className="w-full aspect-video">2</div>
+            <div className="w-full aspect-video">3</div>
+            <div className="w-full aspect-video">4</div>
+        </div>
+        </>
+      );
+  };
+  
+export default Category;
+  
