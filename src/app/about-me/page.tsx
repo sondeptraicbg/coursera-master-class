3f4be729f1402d8187feef4f0f14ea7c4ae364d9
Khanh Viet <vietkhanh1310@gmail.com>
2023-02-22 10:35:16 +0700

    husky

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 const AboutMe = () => {
-  const khanh: string = "ddasdasddsds";
-  return <h1 className="text-3xl font-bold underline">Hello world! {khanh}</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default AboutMe;
