88df075b9869a87079e1be070a1fd53f7d2421d5
Khanh Viet <vietkhanh1310@gmail.com>
2023-02-28 15:58:15 +0700

    meta zoom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "./globals.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <Component {...pageProps} />
+      </Head>
+    </>
+  );
 }
