b59321260b7a8cb5187910932362c96291cd032a
Khanh Viet <vietkhanh1310@gmail.com>
2023-02-28 16:00:24 +0700

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <Component {...pageProps} />
       </Head>
+      <Component {...pageProps} />
     </>
   );
 }
